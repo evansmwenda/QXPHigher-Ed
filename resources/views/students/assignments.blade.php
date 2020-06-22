cf282460144ffa13afff0e5b0319108d7de752ec
evansmwenda <evansmwenda.em@gmail.com>
2020-06-22 11:25:41 +0300

    added assignment submit form

--- resources/views/students/assignments.blade.php
+++ resources/views/students/assignments.blade.php
@@ -18,10 +18,26 @@
                             </h4>
                           </div>
                           <div id="collapse1" class="panel-collapse collapse in">
-                            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.</div>
+                            <div class="panel-body">Assignment on research of the different methods of gathering scientific data<br>
+                            <a href="test_file.zip" download>Download File</a>
+                            <p style="padding-top: 20px;">Once completed, you can submit the assignment from the section below</p>
+                            <form role="form">
+                               <div class="form-group">
+                                <label for="exampleInputFile">File input</label>
+                                <div class="input-group">
+                                  <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="exampleInputFile">
+                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                  </div>
+                                  <div class="input-group-append">
+                                    <span class="input-group-text" id="">Upload</span>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                          </div>
                           </div>
                         </div>
                         <div class="panel panel-default">
