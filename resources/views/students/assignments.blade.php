5825942d467282db4b74bcbf0abd882f5d064f71
evansmwenda <evansmwenda.em@gmail.com>
2020-06-23 17:15:15 +0300

    code cleanup on students dashboard -> assignment

--- resources/views/students/assignments.blade.php
+++ resources/views/students/assignments.blade.php
@@ -27,38 +27,48 @@
                 	<div class="col-sm-10">
                       <div class="panel-group" id="accordion">
 
-                        @foreach($assignments as $assignment)
-                        <div class="panel panel-default">
+                        @if(count($assignments) >0)
+                          @foreach($assignments as $assignment)
+                            <div class="panel panel-default">
+                              <div class="panel-heading">
+                                <h4 class="panel-title">
+                                  <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $assignment->id }}">
+                                  {{ $assignment->course->title}}</a>
+                                </h4>
+                              </div>
+                              <div id="collapse{{ $assignment->id }}" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                  <h4>{{ $assignment->title }}</h4><br>
+                                  {{ $assignment->description }}<br>
+                                <a href="{{url('uploads/assignments/'.$assignment->course->slug.'/'.$assignment->media)}}" download>Download File</a>
+                                <p style="padding-top: 20px;">Once completed, you can submit the assignment from the section below</p>
+                                <form role="form" enctype="multipart/form-data" method="post" action="{{('/assignments')}}"> {{csrf_field() }}
+                                   <div class="form-group">
+                                    <input type="hidden" id="assignment_id" name="assignment_id" value="{{ $assignment->id }}">
+                                    <input type="hidden" id="slug" name="slug" value="{{ $assignment->course->slug }}">
+                                    <label for="exampleInputFile">Choose Assignment</label>
+                                    <div class="input-group">
+                                      <div class="custom-file">
+                                        <input type="file" name="assignment" class="custom-file-input" id="assignment" required>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                              </div>
+                              </div>
+                            </div>
+                          @endforeach
+                        @else
                           <div class="panel-heading">
                             <h4 class="panel-title">
-                              <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $assignment->id }}">
-                              {{ $assignment->course->title}}</a>
+                              <p>You don't have any assignments</p>
                             </h4>
                           </div>
-                          <div id="collapse{{ $assignment->id }}" class="panel-collapse collapse">
-                            <div class="panel-body">
-                              <h4>{{ $assignment->title }}</h4><br>
-                              {{ $assignment->description }}<br>
-                            <a href="{{url('uploads/assignments/'.$assignment->course->slug.'/'.$assignment->media)}}" download>Download File</a>
-                            <p style="padding-top: 20px;">Once completed, you can submit the assignment from the section below</p>
-                            <form role="form" enctype="multipart/form-data" method="post" action="{{('/assignments')}}"> {{csrf_field() }}
-                               <div class="form-group">
-                                <input type="hidden" id="assignment_id" name="assignment_id" value="{{ $assignment->id }}">
-                                <input type="hidden" id="slug" name="slug" value="{{ $assignment->course->slug }}">
-                                <label for="exampleInputFile">Choose Assignment</label>
-                                <div class="input-group">
-                                  <div class="custom-file">
-                                    <input type="file" name="assignment" class="custom-file-input" id="assignment" required>
-                                  </div>
-                                </div>
-                              </div>
+                        @endif
 
-                              <button type="submit" class="btn btn-primary">Submit</button>
-                            </form>
-                          </div>
-                          </div>
-                        </div>
-                        @endforeach
+                        
                       </div>
                     </div>
 
