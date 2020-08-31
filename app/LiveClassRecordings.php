c4dbabb1a8b511667cd3a0560d1d555dce8111ff
evansmwenda <evansmwenda.em@gmail.com>
2020-08-31 13:42:28 +0300

    added live class models

--- app/LiveClassRecordings.php
+++ app/LiveClassRecordings.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class LiveClassRecordings extends Model
 {
     //
+    public $timestamps = false;
+    protected $table = 'live_class_recordings';
+    public $fillable = ['id','meetingID','users'];  
 }
--- app/LiveClasses.php
+++ app/LiveClasses.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class LiveClasses extends Model
 {
     //
+    public $timestamps = false;
+    protected $table = 'live_classes';
+    public $fillable = ['id','title','meetingID','attendeePW','moderatorPW','owner'];  
 }
