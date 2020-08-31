c7fdf1f7b3170c6bc5420d2f2d9bb866cb2404ea
evansmwenda <evansmwenda.em@gmail.com>
2020-08-31 16:47:13 +0300

    created teacher live class blade

--- resources/views/admin/classes/index.blade.php
+++ resources/views/admin/classes/index.blade.php
@@ -27,15 +27,116 @@
         
         <div class="panel-body">
         	<div class="row .d-none .d-xs-block .d-sm-block .d-md-none">
-	            <div class="col-sm-12 col-md-12 text-center">
-	                <button onclick="toggleCreate()" class="btn btn-lg" style="width:150px;background-color:#0734ff;color:white;text-align: center;"><span>Create Meeting</span></button>
-	                <button onclick="toggleJoin()" class="btn btn-lg" style="width:150px;background-color:#0734ff;color:white;text-align: center;"><span>Join Meeting</span></button>
-	                <p style="font-size: 2em;padding-top: 10px;color:#0734ff">&#0149;</p>
+	            <div class="col-sm-12 col-md-12 text-center" style="margin-bottom: 20px;">
+	                <button onclick="toggleCreate()" class="btn btn-primary btn-lg" style="width:150px;color:white;text-align: center;"><span>Create Meeting</span></button>
+	                <button onclick="toggleJoin()" class="btn btn-lg" style="width:150px;background-color:#3c8dbc;color:white;text-align: center;"><span>Join Meeting</span></button>
 
 
 	            </div>
-	          </div>
+	        </div>
+
+	        <div class="login-s" id="toggle-join" style="">
+	        	<div class="container text-center">
+	        		<div class="col-sm-8">
+                		<form class="form-horizontal" 
+                          role="form"
+                          method="POST"
+                          action="/user/dojoin">
+	                        <input type="hidden"
+	                               name="_token"
+	                               value="{{ csrf_token() }}">    
+
+	                        <div class="form-group">
+	                            <label class="col-md-4 control-label">Class ID</label>
+
+	                            <div class="col-md-8">
+	                                <input type="text"
+	                                       class="form-control"
+	                                       name="meetingID"
+	                                       placeholder="Enter Class ID to join" 
+	                                       required>
+	                            </div>
+	                        </div>
+
+
+	                        <div class="form-group">
+	                            <div class="col-md-6 col-md-offset-5">
+	                                <button type="submit"
+	                                        class="btn"
+	                                        style="margin-right: 15px;background-color: #3c8dbc;color:#fff;">
+	                                    JOIN LIVE CLASS
+	                                </button>
+	                            </div>
+	                        </div>
+	                    </form>
+              </div>
+	        	</div>
+              
+          </div>
+
+          <div class="login-s" id="toggle-create" style="display:none">
+              <div class="container text-center">
+                  <div class="col-sm-8">
+                		<form class="form-horizontal" 
+                          role="form"
+                          method="POST"
+                          action="/user/dojoin">
+	                        <input type="hidden"
+	                               name="_token"
+	                               value="{{ csrf_token() }}">    
+
+	                        <div class="form-group">
+	                            <label class="col-md-4 control-label">Title</label>
+
+	                            <div class="col-md-8">
+	                                <input type="text"
+	                                       class="form-control"
+	                                       name="title"
+	                                       placeholder="Enter title" 
+	                                       required>
+	                            </div>
+	                        </div>
+
+
+	                        <div class="form-group">
+	                            <div class="col-md-6 col-md-offset-5">
+	                                <button type="submit"
+	                                        class="btn btn-primary"
+	                                        style="margin-right: 15px;">
+	                                    CREATE LIVE CLASS
+	                                </button>
+	                            </div>
+	                        </div>
+	                    </form>
+              </div>
+              </div>
+          </div>
         </div>
     </div>
     
-    @endsection    
+@endsection 
+@section('javascript')
+    @parent
+<script type="text/javascript">
+    function toggleJoin() {
+        var x = document.getElementById("toggle-join");
+        var y = document.getElementById("toggle-create");
+
+        //check if create element is showing->if yes hide it
+        if (x.style.display === "none") {
+                x.style.display = "block";
+                y.style.display = "none";
+            }
+    }
+    function toggleCreate() {
+        var x = document.getElementById("toggle-join");
+        var y = document.getElementById("toggle-create");
+
+        if (y.style.display === "none") {
+                y.style.display = "block";
+                x.style.display = "none";
+            } 
+    }
+
+</script>
+@stop   
