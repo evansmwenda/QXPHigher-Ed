8b6ee08dbbcbcc0507de04786fd88d7436c886da
evansmwenda <evansmwenda.em@gmail.com>
2020-09-24 14:26:33 +0300

    testing ipn after upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1102,4 +1102,103 @@ class HomeController extends Controller
         // /?pesapal_transaction_tracking_id=058e9adb-d351-4092-9df7-0bd776900859
         // &pesapal_merchant_reference=5f2ad92d9dc87
     }
+    public function getIPNn(){
+        $pesapalMerchantReference = "5f6b3d2b1d637";
+        $transact = MyTransactions::where('reference',$pesapalMerchantReference)->first();
+        dd($transact);
+    }
+
+    public function getIPN(Request $request){
+        /*gets the instant payment notification from pesapal
+        and updates the transactions table*/
+        $pesapalTrackingId          = "";
+        $pesapalNotification        = "";
+        $pesapalMerchantReference   = "";
+        if(isset($request['pesapal_merchant_reference']))
+        $pesapalMerchantReference = $request['pesapal_merchant_reference'];
+        
+        if(isset($request['pesapal_transaction_tracking_id']))
+            $pesapalTrackingId = $request['pesapal_transaction_tracking_id'];
+            
+        if(isset($request['pesapal_notification_type']))
+            $pesapalNotification=$request['pesapal_notification_type'];
+
+
+        $transactionDetails = $this->getTransactionDetails($pesapalMerchantReference,$pesapalTrackingId);
+
+        //End of IPN test
+                
+        //Update database
+        $value  = array("COMPLETED"=>"Paid","PENDING"=>"Pending","INVALID"=>"Cancelled","FAILED"=>"Cancelled");
+        $status = $value[$transactionDetails['status']];
+        
+        $dbUpdateSuccessful = $this->updateTransactionByIPN($transactionDetails);
+
+        ob_start();
+        echo $resp;
+        ob_flush();
+        exit;
+        
+        // if($dbUpdateSuccessful) $dbupdated = "True"; else  $dbupdated = 'False';
+        
+        // //test if IPN runs on status change
+        // $to      = 'evansmwenda.em@gmail.com';
+        // $subject = 'IPN: '.$pesapalNotification;
+        // $message = '<b>Merchant Reference: </b>'.$pesapalMerchantReference.'<br> ';
+        // $message .= '<b>Tracking ID: </b>'.$pesapalTrackingId.'<br> ';
+        // $message .= '<b>Payment Method: </b>'.$transactionDetails['payment_method'].'<br> ';
+        // $message .= '<b>Database update: </b>'.$dbupdated.'<br> ';
+        // $headers = 'From: ipntester@pesapal.com' . "\r\n";
+        // $headers .= 'MIME-Version: 1.0' . "\r\n";
+        // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        // 'Reply-To: no-reply@noreplyx.com' . "\r\n" .
+        // 'X-Mailer: PHP/' . phpversion();
+        
+        // mail($to, $subject, $message, $headers);
+            
+        // //If there was a status change and you updated your db successfully && the change is not to a Pending state 
+        // if($pesapalNotification=="CHANGE"){
+            
+        //     //Notify me when the IPN for this transaction is killed
+        //     $to      = '';
+        //     $subject = 'IPN Killer';
+        //     $message = '<b>Merchant Reference: </b>'.$pesapalMerchantReference.'<br> ';
+        //     $message .= '<b>Tracking ID: </b>'.$pesapalTrackingId.'<br> ';
+        //     $message .= '<b>Status: </b>'.$status.'<br> ';
+        //     $headers = 'From: ipntester@pesapal.com' . "\r\n";
+        //     $headers .= 'MIME-Version: 1.0' . "\r\n";
+        //     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n".
+        //     'Reply-To: no-reply@noreplyx.com' . "\r\n" .
+        //     'X-Mailer: PHP/' . phpversion();
+            
+        //     mail($to, $subject, $message, $headers);
+            
+        //     $resp   = "pesapal_notification_type=$pesapalNotification".     
+        //               "&pesapal_transaction_tracking_id=$pesapalTrackingId".
+        //               "&pesapal_merchant_reference=$pesapalMerchantReference";
+                      
+        //     ob_start();
+        //     echo $resp;
+        //     ob_flush();
+        //     exit; //this is mandatory. If you dont exit, Pesapal will not get your response.
+        // }
+    }
+
+    public function updateTransactionByIPN($transaction){
+
+        $status                     = $transaction['status'];
+        $payment_method             = $transaction['payment_method'];
+        $pesapalMerchantReference   = $transaction['pesapal_merchant_reference'];
+        $pesapalTrackingId          = $transaction['pesapal_transaction_tracking_id'];
+
+        $transact = MyTransactions::where('reference',$pesapalMerchantReference)->first();
+        $transact->status=$status;
+        $transact->payment_method=$payment_method;
+        $transact->tracking_id=$pesapalTrackingId;
+        $transact->reference=$pesapalMerchantReference;
+        $transact->save();
+        
+        return true;
+
+    }
 }
