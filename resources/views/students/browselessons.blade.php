3b9f052edeb9667360db5672639da6b1174af8dc
evansmwenda <evansmwenda.em@gmail.com>
2020-10-22 15:29:30 +0300

    changes made to browse lessons

--- resources/views/students/browselessons.blade.php
+++ resources/views/students/browselessons.blade.php
@@ -2,8 +2,17 @@
 
 @section('main')
 	<div class="row">
-		<div style="background-color: red;height: 160px;">
-			<p>THIS IS THE MY CLASSES</p>
+		<div class="lessons-bg" style="">
+			<div class="lessons-overlay"></div>
+			<div class="container" style="">
+				<div class="row">
+					<div class="col-sm-12 col-md-5">
+						<h1 style="color:#ffffff">My Classes</h1>
+						<h4 style="color:#ffffff">Hello Student, welcome back!</h4>
+					</div>
+				</div>
+			</div>
+			
 		</div>
         <div class="col-md-12 col-sm-12">
         	<h3>Degree Progress Audit</h3>
@@ -14,10 +23,84 @@
         	</p>
         </div>
         <div class="col-sm-12 col-md-7" >
-        	<div class="card border border-info" style="height: 200px;background-color: red;border-radius:20px;">
-        		<p>this is a test</p>
+        	<div class="card" style="border: 1px solid #00CED1;border-radius:6px;width: 100%">
+        		<table class="table table-borderless" style="width: 100%">
+					<thead>
+						<tr style="font-weight: bold;">
+							<td>Course</td>
+							<td>Name</td>
+							<td>Status</td>
+							<td>Term</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Biology 101</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2020-09-12</td>
+						</tr>
+						<tr>
+							<td>BASIC2-D DESIGN</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2019/20/SEM2</td>
+						</tr>
+						<tr>
+							<td>BASIC2-D DESIGN</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2020-09-12</td>
+						</tr>
+						<tr>
+							<td>Biology 101</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2019/20/SEM2</td>
+						</tr>
+					</tbody>
+				</table>
         	</div>
         </div>
-        <div class="col-sm-12 col-md-5"></div>
+        <div class="col-sm-12 col-md-5">
+        	<div class="card" style="border: 1px solid #00CED1;border-radius:6px;width: 100%">
+        		<table class="table table-borderless" style="width: 100%">
+					<thead>
+						<tr style="font-weight: bold;">
+							<td>Course</td>
+							<td>Name</td>
+							<td>Status</td>
+							<td>Term</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Biology 101</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2020-09-12</td>
+						</tr>
+						<tr>
+							<td>BASIC2-D DESIGN</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2019/20/SEM2</td>
+						</tr>
+						<tr>
+							<td>BASIC2-D DESIGN</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2020-09-12</td>
+						</tr>
+						<tr>
+							<td>Biology 101</td>
+							<td>Live Class Meeting</td>
+							<td>Scheduled</td>
+							<td>2019/20/SEM2</td>
+						</tr>
+					</tbody>
+				</table>
+        	</div>
+        </div>
     </div>
 @endsection
