62d10fe72c2a7f04c017edad070538d7fe71d040
geff-mutua <geoffreymutua01@gmail.com>
2020-10-28 16:21:21 +0300

    Live classes design and Examcertification designs

--- resources/views/students/exams.blade.php
+++ resources/views/students/exams.blade.php
@@ -1,135 +1,123 @@
 @extends('layouts.home')
 
 @section('main')
-    <div class="row">
-        <div class="col-md-12">
-          @if(Session::has("flash_message_error")) 
-          <div class="alert alert-error alert-block">
-              <button type="button" class="close" data-dismiss="alert">x</button>
-              <strong>{!! session('flash_message_error') !!}</strong>
-          </div> 
-          @endif 
-
-          @if(Session::has("flash_message_success")) 
-              <div class="alert alert-info alert-block">
-                  <button type="button" class="close" data-dismiss="alert">x</button>
-                  <strong>{!! session('flash_message_success') !!}</strong>
-              </div> 
-          @endif
-            <div class="panel panel-default">
-                <div class="panel-heading">Available Exams</div>
-
-                <div class="panel-body">
-
-                	<div class="col-sm-12">
-                    <div class="panel-group" id="accordion">
-
-                        @if(count($exams) >0)
-                         
-                            <div class="panel panel-default">
-                              <div class="panel-heading">
-                                <table class="table table-striped">
-                                  <tr>
-                                    <thead class="bg-info">
-                                      <td>#</td>
-                                      <td>Course Name</td>
-                                      <td>Exam Title</td>
-                                      <td>Attempt</td>
-                                    </thead>
-                                    <tbody>
-                                      @foreach($exams as $exam)
-                                      <tr>
-                                        <td>{{ $exam->id }}</td>
-                                        <td>{{ $exam->course->title}}</td>
-                                        <td>{{ $exam->title}}</td>
-                                        <td>
-                                          <a href="{{url('/exams/save/'.$exam->id)}}">Attempt Exam</a>
-                                        </td>
-                                      </tr>
-                                      @endforeach
-                                    </tbody>
-                                  </tr>
-                                </table>
-
-                              </div>
-                            </div>
-                         
-                        @else
-                          <div class="panel-heading">
-                            <h4 class="panel-title">
-                              <p>You don't have any exams</p>
-                            </h4>
-                          </div>
-                        @endif
-
-                        
-                      </div>
-                      <!-- radio -->
-                      <!-- <div class="form-group">
-                      	<p>1. Which one of the following animals is known for strength?</p>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1">
-                          <label class="form-check-label">Elephant</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1">
-                          <label class="form-check-label">Hippopotamus</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1">
-                          <label class="form-check-label">Buffalo</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1">
-                          <label class="form-check-label">Shark</label>
-                        </div>
-                      </div>
-
-                      <div class="form-group">
-                      	<p>2. Which one of the following animals is known for speed?</p>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio2">
-                          <label class="form-check-label">Bear</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio2">
-                          <label class="form-check-label">Cheetah</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio2">
-                          <label class="form-check-label">Leopard</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio2">
-                          <label class="form-check-label">Eagle</label>
-                        </div>
-                      </div>
-
-                      <div class="form-group">
-                      	<p>3. Which one of the following animals is known for faithfulness?</p>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio3">
-                          <label class="form-check-label">Antelope</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio3">
-                          <label class="form-check-label">Dog</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio3">
-                          <label class="form-check-label">Rabbit</label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio3">
-                          <label class="form-check-label">Elephant</label>
-                        </div>
-                      </div> -->
-                    </div>
-
-
-                    
+<div class="row">
+  <div class="col-md-8">
+    <div class="row top-header-2">
+        <div class="col-md-12 col-sm-12" >
+            <div class="col-sm-6">
+                <div class="form-group has-search">
+                    <input type="text" class="form-control" placeholder="Search">
                 </div>
             </div>
+            <div class="col-sm-2">
+                <span class="fa fa-shield-alt fa-2x"></span>
+            </div>
+            <div class="col-sm-2">
+                <span class="fa fa-bell fa-2x"></span>
+            </div>
+            <div class="col-sm-2">
+                <span class="fa fa-calendar-alt fa-2x"></span>
+            </div>
+        </div> 
+    </div>
+  </div>
+  <div class="col-md-4 dashboard-right">
+    <div class="row top-right">
+        <i class="fa fa-user fa-2x"></i> 
+            <a href="#" class="sidebar-toggle pull-right" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="fa fa-bars"></span>
+            </a> 
+    </div>
+  </div>
+</div>
+{{-- content  --}}
+
+<div class="Row">
+  <div class="row exam-top">
+    <div class="exam-overlay">
+        @if(Session::has("flash_message_error")) 
+        <div class="alert alert-error alert-block">
+            <button type="button" class="close" data-dismiss="alert">x</button>
+            <strong>{!! session('flash_message_error') !!}</strong>
+        </div> 
+        @endif 
+
+        @if(Session::has("flash_message_success")) 
+            <div class="alert alert-info alert-block">
+                <button type="button" class="close" data-dismiss="alert">x</button>
+                <strong>{!! session('flash_message_success') !!}</strong>
+            </div> 
+        @endif
+        <h2>Hello student</h2>
+        <p>Your education is your power. So learn more and more</p>
+        <span><strong>Good luck in your exams today</strong></span>
+    </div>
+  </div>
+
+{{-- main content --}}
+    <div class="row col-md-12" style="background: #fff">
+      <h3 style="color: #060646; font-weight:600">Scheduled Exams</h3>
+      @if(count($exams) >0)
+      <div class="row">
+        @foreach($exams as $exam)
+          <div class="exam-card">
+            <div class="exam-header">
+              {{ $exam->title}}
+            </div>
+            <p>{{ $exam->course->title}}</p>
+              <span>Author:</span><br>
+              <i>Dr, Harry Garza</i>
+              <h4 class="pull-right">30th November,2020</h4>
+          </div>
+         @endforeach
+
+      </div>
+      <div class="row">
+        {{ $exams->links() }}
+       </div>
+      @else
+        <div class="row">
+          <h3 class="text-center"> No exam Scheduled</h3>
+        </div>
+      @endif
+      <div class="exam-warning">
+        <div class="row">
+          <div class="col-md-2 exam-fa text-center">
+            <span class="fa fa-user fa-2x"></span>
+          </div>
+          <div class="col-md-9 exam2">
+            <h3>Carefully read through the exam instructiosn prior to starting</h3>
+            <i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea magnam repellendus optio magni commodi possimus.</i>
+          </div>
         </div>
     </div>
+    {{-- Submitted exams --}}
+    <h3 style="color: #060646; font-weight:600">Submitted Exams</h3>
+     
+    @if(count($exams) >0)
+    <div class="row">
+      @foreach($exams as $exam)
+        <div class="exam-card">
+          <div class="exam-header">
+            {{ $exam->title}}
+          </div>
+          <p>{{ $exam->course->title}}</p>
+            <span>Author:</span><br>
+            <i>Dr, Harry Garza</i>
+            <h4 class="pull-right">30th November,2020</h4>
+        </div>
+       @endforeach
+
+    </div>
+    <div class="row">
+      {{ $exams->links() }}
+     </div>
+    @else
+      <div class="row">
+        <h3 class="text-center"> No exam Scheduled</h3>
+      </div>
+    @endif
+</div>
 @endsection
