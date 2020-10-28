610cf3e4457370ee640650758c465d4c1c6d187a
evansmwenda <evansmwenda.em@gmail.com>
2020-10-28 16:20:56 +0300

    account,assignments and browse lesson done

--- resources/views/students/assignments.blade.php
+++ resources/views/students/assignments.blade.php
@@ -23,22 +23,6 @@
 	
   </div>
 
-  {{-- @foreach($assignments as $assignment)
-  <tr>
-    <td>{{ $assignment->id }}</td>
-   <td>{{ $assignment->course->title}}</td>
-   <td>{{ $assignment->title}}</td>
-   <td>Due Date</td>
-   <td>
-     <a class="btn btn-primary btn-sm" href="{{ $assignment->id }}">
-       <i class="fas fa-folder">
-       </i>
-       View
-   </a>
-   </td>
-  </tr>
-  @endforeach --}}
-
   @if(count($assignments) > 0)
     @foreach($assignments as $assignment)
       <div class="assignment-card">
@@ -56,7 +40,6 @@
       </div>
     @endforeach
   @else
-    {{-- //no assignments --}}
     <p class="text-center">You have no upcoming assignments</p>
   @endif
 
@@ -77,84 +60,4 @@
 	   </div>
 	   {{-- @include('partials.recentactivity') --}}
   </div>  
-  
-
-
-
-
-    {{-- <div class="row">
-        <div class="col-md-12">
-
-          @if(Session::has("flash_message_error")) 
-          <div class="alert alert-error alert-block">
-              <button type="button" class="close" data-dismiss="alert">x</button>
-              <strong>{!! session('flash_message_error') !!}</strong>
-          </div> 
-          @endif 
-
-          @if(Session::has("flash_message_success")) 
-              <div class="alert alert-success alert-block">
-                  <button type="button" class="close" data-dismiss="alert">x</button>
-                  <strong>{!! session('flash_message_success') !!}</strong>
-              </div> 
-          @endif
-
-
-            <div class="panel panel-default">
-                <div class="panel-heading">My Assignments </div>
-
-                <div class="panel-body">
-
-                	<div class="col-sm-12">
-                      <div class="panel-group" id="accordion">
-
-                        @if(count($assignments) >0)
-                         
-                           <table class="table table-bordered table-striped">
-                             <tr>
-                               <thead class="bg-info">
-                                 <td>#</td>
-                                 <td>Course Name</td>
-                                 <td>Assignment Title</td>
-                                 <td>Date Due</td>
-                                 <td>Action</td>
-                               </thead>
-                             </tr>
-                             <tbody>
-                              @foreach($assignments as $assignment)
-                               <tr>
-                                 <td>{{ $assignment->id }}</td>
-                                <td>{{ $assignment->course->title}}</td>
-                                <td>{{ $assignment->title}}</td>
-                                <td>Due Date</td>
-                                <td>
-                                  <a class="btn btn-primary btn-sm" href="{{ $assignment->id }}">
-                                    <i class="fas fa-folder">
-                                    </i>
-                                    View
-                                </a>
-                                </td>
-                               </tr>
-                               @endforeach
-                             </tbody>
-                           </table>
-                         
-                        @else
-                          <div class="panel-heading">
-                            <h4 class="panel-title">
-                              <p>You don't have any assignments</p>
-                            </h4>
-                          </div>
-                        @endif
-
-                        
-                      </div>
-                    </div>
-
-
-                    
-                </div>
-            </div>
-        </div>
-    </div> --}}
 @endsection
