87d5bac7375fb36a7b1ccb539e9fbd912aa3736d
geff-mutua <geoffreymutua01@gmail.com>
2020-10-29 10:35:05 +0300

    Resosiveness on mobile devices

--- resources/views/students/exams.blade.php
+++ resources/views/students/exams.blade.php
@@ -2,35 +2,7 @@
 
 @section('main')
 <div class="row">
-  <div class="col-md-8">
-    <div class="row top-header-2">
-        <div class="col-md-12 col-sm-12" >
-            <div class="col-sm-6">
-                <div class="form-group has-search">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-            </div>
-            <div class="col-sm-2">
-                <span class="fa fa-shield-alt fa-2x"></span>
-            </div>
-            <div class="col-sm-2">
-                <span class="fa fa-bell fa-2x"></span>
-            </div>
-            <div class="col-sm-2">
-                <span class="fa fa-calendar-alt fa-2x"></span>
-            </div>
-        </div> 
-    </div>
-  </div>
-  <div class="col-md-4 dashboard-right">
-    <div class="row top-right">
-        <i class="fa fa-user fa-2x"></i> 
-            <a href="#" class="sidebar-toggle pull-right" data-toggle="offcanvas" role="button">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="fa fa-bars"></span>
-            </a> 
-    </div>
-  </div>
+ @include('students.header')
 </div>
 {{-- content  --}}
 
@@ -62,7 +34,7 @@
       @if(count($exams) >0)
       <div class="row">
         @foreach($exams as $exam)
-          <div class="exam-card">
+          <div class="exam-card" onclick="location.href='{{url('/exams/save/'.$exam->id)}}';">
             <div class="exam-header">
               {{ $exam->title}}
             </div>
@@ -72,7 +44,6 @@
               <h4 class="pull-right">30th November,2020</h4>
           </div>
          @endforeach
-
       </div>
       <div class="row">
         {{ $exams->links() }}
